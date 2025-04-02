cbb2d3a2609905e18dd4197074b8791e20509d9d
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-04-02 21:29:26 +0500

    redirect authenticated users to dashboard from home route

--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
     return render_template('index.html')
 
 if __name__ == "__main__":
